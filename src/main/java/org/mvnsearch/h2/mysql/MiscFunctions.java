b57618f371529aacdc1cde37d387f4cd94272bee
linux_china <linux_china@hotmail.com>
2017-12-28 19:37:40 +0800

    misc functions

---
+++ src/main/java/org/mvnsearch/h2/mysql/MiscFunctions.java
@@ -0,0 +1,13 @@
+package org.mvnsearch.h2.mysql;
+
+/**
+ * misc functions
+ *
+ * @author linux_china
+ */
+public class MiscFunctions {
+
+    public static String version() {
+        return "5.6.25";
+    }
+}
