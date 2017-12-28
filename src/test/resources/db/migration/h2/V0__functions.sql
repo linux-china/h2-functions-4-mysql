c6f1db1fc0b995d6da5083fee6194e3c813b352d
linux_china <linux_china@hotmail.com>
2017-12-28 19:15:36 +0800

    add FROM_UNIXTIME

--- src/test/resources/db/migration/h2/V0__functions.sql
+++ src/test/resources/db/migration/h2/V0__functions.sql
@@ -1,1 +1,2 @@
-CREATE ALIAS UNIX_TIMESTAMP FOR "org.mvnsearch.h2.mysql.Functions.unixTimestamp"
+CREATE ALIAS UNIX_TIMESTAMP FOR "org.mvnsearch.h2.mysql.Functions.unixTimestamp";
+CREATE ALIAS FROM_UNIXTIME FOR "org.mvnsearch.h2.mysql.Functions.fromUnixTime";
