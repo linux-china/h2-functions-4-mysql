4d853f2c1d5d86e92ad9dec401245952a5e27fa8
linux_china <linux_china@hotmail.com>
2017-12-28 19:57:42 +0800

    add functions

--- src/test/resources/db/migration/h2/V0__functions.sql
+++ src/test/resources/db/migration/h2/V0__functions.sql
@@ -1,3 +1,8 @@
 CREATE ALIAS UNIX_TIMESTAMP FOR "org.mvnsearch.h2.mysql.DateTimeFunctions.unixTimestamp";
 CREATE ALIAS FROM_UNIXTIME FOR "org.mvnsearch.h2.mysql.DateTimeFunctions.fromUnixTime";
 CREATE ALIAS VERSION FOR "org.mvnsearch.h2.mysql.MiscFunctions.version";
+CREATE ALIAS MD5 FOR "org.mvnsearch.h2.mysql.MiscFunctions.md5";
+CREATE ALIAS SHA1 FOR "org.mvnsearch.h2.mysql.MiscFunctions.sha1";
+CREATE ALIAS HEX FOR "org.mvnsearch.h2.mysql.MiscFunctions.hex";
+CREATE ALIAS UNHEX FOR "org.mvnsearch.h2.mysql.MiscFunctions.unhex";
+CREATE ALIAS PASSWORD FOR "org.mvnsearch.h2.mysql.MiscFunctions.password";
