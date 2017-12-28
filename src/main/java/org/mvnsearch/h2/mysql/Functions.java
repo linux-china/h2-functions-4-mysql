d2e6d239bd12f3ac258cdfa8aad881817f63d11a
linux_china <linux_china@hotmail.com>
2017-12-28 19:15:24 +0800

    add unixTimestamp(String text)

--- src/main/java/org/mvnsearch/h2/mysql/Functions.java
+++ src/main/java/org/mvnsearch/h2/mysql/Functions.java
@@ -1,5 +1,8 @@
 package org.mvnsearch.h2.mysql;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * h2 use defined functions for MySQL
  *
@@ -14,5 +17,19 @@ public class Functions {
      */
     public static Long unixTimestamp() {
         return System.currentTimeMillis();
+    }
+
+    /**
+     * function for UNIX_TIMESTAMP
+     *
+     * @return current time millis
+     */
+    public static Long unixTimestamp(String text) throws Exception {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("YYYY-MM-DD HH:MM:SS");
+        return dateFormat.parse(text).getTime();
+    }
+
+    public static Date fromUnixTime(Long unixTime) {
+        return new Date(unixTime * 1000);
     }
 }
