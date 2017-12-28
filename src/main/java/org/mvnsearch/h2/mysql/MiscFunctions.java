404704d7713211202725744559e746ace12c21db
linux_china <linux_china@hotmail.com>
2017-12-28 20:01:14 +0800

    add functions

--- src/main/java/org/mvnsearch/h2/mysql/MiscFunctions.java
+++ src/main/java/org/mvnsearch/h2/mysql/MiscFunctions.java
@@ -1,5 +1,8 @@
 package org.mvnsearch.h2.mysql;
 
+import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.codec.digest.DigestUtils;
+
 /**
  * misc functions
  *
@@ -10,4 +13,26 @@ public class MiscFunctions {
     public static String version() {
         return "5.6.25";
     }
+
+    public static String md5(String text) {
+        return DigestUtils.md5Hex(text);
+    }
+
+    public static String sha1(String text) {
+        return DigestUtils.sha1Hex(text);
+    }
+
+    public static String hex(String text) {
+        return Hex.encodeHexString(text.getBytes());
+    }
+
+    public static String unhex(String text) throws Exception {
+        return new String(Hex.decodeHex(text));
+    }
+
+    public static String password(String text) throws Exception {
+        return "*" + sha1(unhex(sha1(text)));
+    }
+
+
 }
