d4b5729634db9df7eac1e64736d981e4d1e7ad85
linux_china <linux_china@hotmail.com>
2018-05-16 15:21:44 -0700

    add encrypt related functions

--- src/main/java/org/mvnsearch/h2/mysql/EncryptionFunctions.java
+++ src/main/java/org/mvnsearch/h2/mysql/EncryptionFunctions.java
@@ -1,5 +1,7 @@
 package org.mvnsearch.h2.mysql;
 
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.codec.digest.DigestUtils;
 
 import static org.apache.commons.codec.digest.MessageDigestAlgorithms.SHA_224;
@@ -11,18 +13,43 @@ import static org.apache.commons.codec.digest.MessageDigestAlgorithms.SHA_224;
  */
 public class EncryptionFunctions {
 
-    public static String SHA1(String text) {
+
+    public static String hex(String text) {
+        return Hex.encodeHexString(text.getBytes());
+    }
+
+    public static String unhex(String text) throws Exception {
+        return new String(Hex.decodeHex(text));
+    }
+
+    public static String password(String text) throws Exception {
+        return "*" + sha1(unhex(sha1(text)));
+    }
+
+    public static String base64(String text) throws Exception {
+        return Base64.encodeBase64String(text.getBytes());
+    }
+
+    public static String unbase64(String text) throws Exception {
+        return new String(Base64.decodeBase64(text.getBytes()));
+    }
+
+    public static String md5(String text) {
+        return DigestUtils.md5Hex(text);
+    }
+
+    public static String sha1(String text) {
         if (text == null || text.isEmpty()) {
             return text;
         }
         return DigestUtils.sha1Hex(text);
     }
 
-    public static String SHA(String text) {
-        return SHA1(text);
+    public static String sha(String text) {
+        return sha1(text);
     }
 
-    public static String SHA2(String text, Integer hashLength) throws Exception {
+    public static String sha2(String text, Integer hashLength) throws Exception {
         if (hashLength == 0 || hashLength == 256) {
             return DigestUtils.sha256Hex(text);
         } else if (hashLength == 224) {
