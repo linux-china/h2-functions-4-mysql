0b2b2979eb12ad0f765427f8d68c0261101474bf
linux_china <linux_china@hotmail.com>
2018-05-16 15:15:50 -0700

    Encryption functions

---
+++ src/main/java/org/mvnsearch/h2/mysql/EncryptionFunctions.java
@@ -0,0 +1,33 @@
+package org.mvnsearch.h2.mysql;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+import static org.apache.commons.codec.digest.MessageDigestAlgorithms.SHA_224;
+
+/**
+ * Encryption functions
+ *
+ * @author linux_china
+ */
+public class EncryptionFunctions {
+
+    public static String SHA1(String text) {
+        if (text == null || text.isEmpty()) {
+            return text;
+        }
+        return DigestUtils.sha1Hex(text);
+    }
+
+    public static String SHA2(String text, Integer hashLength) throws Exception {
+        if (hashLength == 0 || hashLength == 256) {
+            return DigestUtils.sha256Hex(text);
+        } else if (hashLength == 224) {
+            return new DigestUtils(SHA_224).digestAsHex(text);
+        } else if (hashLength == 384) {
+            return DigestUtils.sha384Hex(text);
+        } else if (hashLength == 512) {
+            return DigestUtils.sha512Hex(text);
+        }
+        return null;
+    }
+}
