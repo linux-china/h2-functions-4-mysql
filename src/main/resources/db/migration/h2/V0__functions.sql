db81a1affbcc6b9d81019facff9915e0c6b60178
linux_china <linux_china@hotmail.com>
2018-05-16 15:22:15 -0700

    adjust encrypt related functions

--- src/main/resources/db/migration/h2/V0__functions.sql
+++ src/main/resources/db/migration/h2/V0__functions.sql
@@ -1,13 +1,17 @@
+-- date & time
 CREATE ALIAS UNIX_TIMESTAMP FOR "org.mvnsearch.h2.mysql.DateTimeFunctions.unixTimestamp";
 CREATE ALIAS FROM_UNIXTIME FOR "org.mvnsearch.h2.mysql.DateTimeFunctions.fromUnixTime";
 CREATE ALIAS ADDDATE FOR "org.mvnsearch.h2.mysql.DateTimeFunctions.addDate";
 CREATE ALIAS ADDTIME FOR "org.mvnsearch.h2.mysql.DateTimeFunctions.addTime";
-CREATE ALIAS VERSION FOR "org.mvnsearch.h2.mysql.MiscFunctions.version";
-CREATE ALIAS MD5 FOR "org.mvnsearch.h2.mysql.MiscFunctions.md5";
-CREATE ALIAS SHA1 FOR "org.mvnsearch.h2.mysql.MiscFunctions.sha1";
-CREATE ALIAS HEX FOR "org.mvnsearch.h2.mysql.MiscFunctions.hex";
-CREATE ALIAS UNHEX FOR "org.mvnsearch.h2.mysql.MiscFunctions.unhex";
-CREATE ALIAS PASSWORD FOR "org.mvnsearch.h2.mysql.MiscFunctions.password";
-CREATE ALIAS TO_BASE64 FOR "org.mvnsearch.h2.mysql.MiscFunctions.base64";
-CREATE ALIAS FROM_BASE64 FOR "org.mvnsearch.h2.mysql.MiscFunctions.unbase64";
+-- encrypt
+CREATE ALIAS MD5 FOR "org.mvnsearch.h2.mysql.EncryptionFunctions.md5";
+CREATE ALIAS SHA1 FOR "org.mvnsearch.h2.mysql.EncryptionFunctions.sha1";
+CREATE ALIAS SHA FOR "org.mvnsearch.h2.mysql.EncryptionFunctions.sha";
+CREATE ALIAS HEX FOR "org.mvnsearch.h2.mysql.EncryptionFunctions.hex";
+CREATE ALIAS UNHEX FOR "org.mvnsearch.h2.mysql.EncryptionFunctions.unhex";
+CREATE ALIAS PASSWORD FOR "org.mvnsearch.h2.mysql.EncryptionFunctions.password";
+CREATE ALIAS TO_BASE64 FOR "org.mvnsearch.h2.mysql.EncryptionFunctions.base64";
+CREATE ALIAS FROM_BASE64 FOR "org.mvnsearch.h2.mysql.EncryptionFunctions.unbase64";
+-- misc
 CREATE ALIAS FORMAT FOR "org.mvnsearch.h2.mysql.MiscFunctions.format";
+CREATE ALIAS VERSION FOR "org.mvnsearch.h2.mysql.MiscFunctions.version";
