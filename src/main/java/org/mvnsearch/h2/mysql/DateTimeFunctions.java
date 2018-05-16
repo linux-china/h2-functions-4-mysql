233fd230cebfe3a6efb8cea5414e3139ad1ee27b
linux_china <linux_china@hotmail.com>
2018-05-16 14:23:08 -0700

    add addTime

--- src/main/java/org/mvnsearch/h2/mysql/DateTimeFunctions.java
+++ src/main/java/org/mvnsearch/h2/mysql/DateTimeFunctions.java
@@ -2,7 +2,6 @@ package org.mvnsearch.h2.mysql;
 
 import org.apache.commons.lang3.time.DateUtils;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
@@ -37,5 +36,11 @@ public class DateTimeFunctions {
     public static Date addDate(String dateText, Integer days) throws Exception {
         Date date = DateUtils.parseDate(dateText, "yyyy-MM-dd");
         return new Date(date.getTime() + days * 24 * 60 * 60 * 1000);
+    }
+
+    public static Date addTime(String dateText, String timeText) throws Exception {
+        Date date = DateUtils.parseDate(dateText, "yyyy-MM-dd", "yyyy-MM-dd HH:mm:ss", "yyyy-MM-dd HH:mm:ss.S", "HH:mm:ss", "HH:mm:ss.S", "dd HH:mm:ss", "dd HH:mm:ss.S");
+        Date time = DateUtils.parseDate(timeText, "HH:mm:ss", "HH:mm:ss.S", "dd HH:mm:ss", "dd HH:mm:ss.S");
+        return new Date(date.getTime() + time.getTime());
     }
 }
