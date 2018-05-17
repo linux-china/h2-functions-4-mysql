97d192b924330a4466bf2f2daaed3c9fda7c52d8
linux_china <linux_china@hotmail.com>
2018-05-17 12:13:58 -0700

    add time format

--- src/main/resources/db/migration/h2/V0__functions.sql
+++ src/main/resources/db/migration/h2/V0__functions.sql
@@ -13,6 +13,7 @@ CREATE ALIAS TO_DAYS FOR "org.mvnsearch.h2.mysql.DateTimeFunctions.toDays";
 CREATE ALIAS TO_SECONDS FOR "org.mvnsearch.h2.mysql.DateTimeFunctions.toSeconds";
 CREATE ALIAS TIME_TO_SEC FOR "org.mvnsearch.h2.mysql.DateTimeFunctions.timeToSeconds";
 CREATE ALIAS DATE_FORMAT FOR "org.mvnsearch.h2.mysql.DateTimeFunctions.dateFormat";
+CREATE ALIAS TIME_FORMAT FOR "org.mvnsearch.h2.mysql.DateTimeFunctions.timeFormat";
 CREATE ALIAS LAST_DAY FOR "org.mvnsearch.h2.mysql.DateTimeFunctions.lastDay";
 CREATE ALIAS LOCALTIME FOR "org.mvnsearch.h2.mysql.DateTimeFunctions.now";
 CREATE ALIAS LOCALTIMESTAMP FOR "org.mvnsearch.h2.mysql.DateTimeFunctions.now";
@@ -24,6 +25,8 @@ CREATE ALIAS STR_TO_DATE FOR "org.mvnsearch.h2.mysql.DateTimeFunctions.strToDate
 CREATE ALIAS SUBDATE FOR "org.mvnsearch.h2.mysql.DateTimeFunctions.subDate";
 CREATE ALIAS SUBTIME FOR "org.mvnsearch.h2.mysql.DateTimeFunctions.subTime";
 CREATE ALIAS YEARWEEK FOR "org.mvnsearch.h2.mysql.DateTimeFunctions.yearWeek";
+CREATE ALIAS WEEKOFYEAR FOR "org.mvnsearch.h2.mysql.DateTimeFunctions.weekOfYear";
+CREATE ALIAS WEEKDAY FOR "org.mvnsearch.h2.mysql.DateTimeFunctions.weekDay";
 -- encrypt
 CREATE ALIAS MD5 FOR "org.mvnsearch.h2.mysql.EncryptionFunctions.md5";
 CREATE ALIAS SHA1 FOR "org.mvnsearch.h2.mysql.EncryptionFunctions.sha1";
@@ -46,10 +49,11 @@ CREATE ALIAS IS_UUID FOR "org.mvnsearch.h2.mysql.StringFunctions.isUUID";
 CREATE ALIAS REVERSE FOR "org.mvnsearch.h2.mysql.StringFunctions.reverse";
 CREATE ALIAS SUBSTRING_INDEX FOR "org.mvnsearch.h2.mysql.StringFunctions.subStringIndex";
 CREATE ALIAS STRCMP FOR "org.mvnsearch.h2.mysql.StringFunctions.strCmp";
-CREATE ALIAS CONNECTION_ID FOR "org.mvnsearch.h2.mysql.MiscFunctions.connectId";
-CREATE ALIAS SESSION_USER FOR "org.mvnsearch.h2.mysql.MiscFunctions.sessionUser";
 -- math functions
 CREATE ALIAS POW FOR "org.mvnsearch.h2.mysql.MathFunctions.pow";
 -- misc
 CREATE ALIAS FORMAT FOR "org.mvnsearch.h2.mysql.MiscFunctions.format";
 CREATE ALIAS VERSION FOR "org.mvnsearch.h2.mysql.MiscFunctions.version";
+CREATE ALIAS CONNECTION_ID FOR "org.mvnsearch.h2.mysql.MiscFunctions.connectId";
+CREATE ALIAS SESSION_USER FOR "org.mvnsearch.h2.mysql.MiscFunctions.sessionUser";
+
