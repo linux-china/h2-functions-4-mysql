5ab0e50526f16c05dacbda1e8c1d2ab6dccbbc70
linux_china <linux_china@hotmail.com>
2018-05-16 22:39:25 -0700

    add findInSet

--- src/main/java/org/mvnsearch/h2/mysql/StringFunctions.java
+++ src/main/java/org/mvnsearch/h2/mysql/StringFunctions.java
@@ -2,6 +2,8 @@ package org.mvnsearch.h2.mysql;
 
 import org.apache.commons.lang3.RandomUtils;
 
+import java.util.Arrays;
+
 /**
  * String functions
  *
@@ -15,5 +17,11 @@ public class StringFunctions {
 
     public static Long uuidShort() {
         return RandomUtils.nextLong(Long.MAX_VALUE / 2, Long.MAX_VALUE);
+    }
+
+    public static Integer findInSet(String str, String strSet) {
+        if (str == null || strSet == null) return null;
+        if (strSet.isEmpty()) return 0;
+        return Arrays.asList(strSet.split(",")).indexOf(str) + 1;
     }
 }
