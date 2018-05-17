7a2870a479467246f45d752d9e2e1a843e98deae
linux_china <linux_china@hotmail.com>
2018-05-16 17:14:43 -0700

    String functions

---
+++ src/main/java/org/mvnsearch/h2/mysql/StringFunctions.java
@@ -0,0 +1,19 @@
+package org.mvnsearch.h2.mysql;
+
+import java.util.UUID;
+
+/**
+ * String functions
+ *
+ * @author linux_china
+ */
+public class StringFunctions {
+
+    public static String bin(Long number) {
+        return Long.toBinaryString(number);
+    }
+
+    public static String uuid() {
+        return UUID.randomUUID().toString();
+    }
+}
